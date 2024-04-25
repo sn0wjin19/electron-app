9976cb08c3bc69b4e67bb9020724d76baedb8717
zhangjinfang <zhangjinfang@bytedance.com>
2024-04-25 18:01:55 +0800

    save

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -8,32 +8,37 @@ import { Home, LayoutDashboard, CookingPot } from 'lucide-react';
 
 function Hello() {
   return (
-    <div>
-      <button type="button" className="btn btn-primary">
-        Primary
-      </button>
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default function App() {
-  // const dashboardMatch = useRouteMatch("/");
 
   return (
     <Router>
       <Background>
-        {/* <NavBar /> */}
         <div className="flex">
           <Sidebar>
             <SidebarItem icon={<LayoutDashboard size={20} />} text={"Dashboard"} link={"/"} />
             <SidebarItem icon={<Home size={20} />} text={"Home"} link={"/home"} />
             <SidebarItem icon={<CookingPot size={20} />} text={"Cooking"} link={"/cooking"} />
           </Sidebar>
-          <Routes>
-            <Route path="/" element={<Hello />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/cooking" element={<Cooking />} />
-          </Routes>
+          <div id="bigRoundPanel" className='hero h-3/5 justify-center'>
+            <Routes>
+                <Route path="/" element={<Hello />} />
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/cooking" element={<Cooking />} />
+            </Routes>
+          </div>
         </div>
       </Background>
     </Router>
