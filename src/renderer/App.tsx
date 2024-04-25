dfb9f0dd561e83497631884f949932783a11f8c2
sn0wjin19 <sn0wjin19@gmail.com>
2024-04-25 21:40:26 +0800

    save

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -11,10 +11,10 @@ function Hello() {
     <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
-        <div className="max-w-md">
+        <div className="max-w-md text-white">
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
           <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-primary text-white">Get Started</button>
         </div>
       </div>
     </div>
@@ -32,7 +32,7 @@ export default function App() {
             <SidebarItem icon={<Home size={20} />} text={"Home"} link={"/home"} />
             <SidebarItem icon={<CookingPot size={20} />} text={"Cooking"} link={"/cooking"} />
           </Sidebar>
-          <div id="bigRoundPanel" className='hero h-3/5 justify-center'>
+          <div id="bigRoundPanel" className='m-4 w-screen rounded-lg bg-white'>
             <Routes>
                 <Route path="/" element={<Hello />} />
                 <Route path="/home" element={<HomePage />} />
--- src/renderer/Page/Cooking/Cooking.tsx
+++ src/renderer/Page/Cooking/Cooking.tsx
@@ -2,8 +2,9 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div>
-            <p>sss</p>
+        <div className="flex flex-col m-10">
+            <h1 className="font-bold text-black pb-4 font-sans text-4xl">食谱工具</h1>
+            <h2>sss</h2>
         </div>
     )
 }
